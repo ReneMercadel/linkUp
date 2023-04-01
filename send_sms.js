a4b496f99f8d49f425ecd193af3c5f03bc73857d
Rene Mercadel <renemercadel@gmail.com>
2023-04-01 16:26:55 -0500

    Successfully Accessed SMS Data in Node Server

--- send_sms.js
+++ send_sms.js
@@ -2,43 +2,48 @@
 // Find your Account SID and Auth Token at twilio.com/console
 // and set the environment variables. See http://twil.io/secure
 
-//  TO RUN THIS PAGE: 
+//  TO RUN THIS PAGE:
 //  ngrok http 3000 (to connect to https://6854-174-76-80-43.ngrok.io)
 //  node send_sms.js (to run on 3000)
-// 
+//
 require('dotenv').config();
+
+// body parser to parse encoded message data
+const bodyParser = require('body-parser');
 
 // required libraries for node.js
 const express = require('express');
 const app = express();
 const port = 3000;
 
-
 const accountSid = process.env.TWILIO_SID;
 const authToken = process.env.TWILIO_TOKEN;
 // const twilioNumber= process.env.TWILIO_NUMBER;
 
-
-
-// required modules for twilio response 
+// required modules for twilio response
 const client = require('twilio')(accountSid, authToken);
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
+// Configure body-parser middleware for urlencoded data
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// webhook to handle response 
+// webhook to handle response
+app.post('/message-rene', async (req, res) => {
+  console.log(`req`, req.body.Body);
 
-app.post('/message', async (req, res) => {
+  let textResponse;
+  if (req.body.Body) {
+    twiml.message('this is a message from us made in node');
+  }
+
   const twiml = new MessagingResponse();
-  twiml.message('this is a message from us made in node')
-  res.writeHead(200,{'Content-Type': 'text/xml'});
-  res.end(twiml.toString())
-}); 
-
+  res.writeHead(200, { 'Content-Type': 'text/xml' });
+  res.end(twiml.toString());
+});
 
 app.listen(3000, () => {
   console.log('Express server listening on port 3000');
 });
-
 
 // client.messages
 //   .create({
