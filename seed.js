3d1b9b88ddfbc396db142825d6a85cadd2f63cf7
Rene Mercadel <renemercadel@gmail.com>
2023-04-01 20:59:07 -0500

    Added Static GET Request to Retrieve some Dummy Food Posts, Added Nodemon and Start Script to Dependencies. Attempting to Format Simple Twilio POST Response.

---
+++ seed.js
@@ -0,0 +1,73 @@
+// const faker = require('faker');
+import { faker } from '@faker-js/faker';
+
+const seedArr = [];
+
+const resourceArray = [
+  'Food',
+  'Shelter',
+  'Transportation',
+  'Education',
+  'Childcare',
+  'Health',
+];
+
+const nolaBoundaries = {
+  minLatitude: 29.89,
+  maxLatitude: 30.07,
+  minLongitude: -90.14,
+  maxLongitude: -89.88,
+};
+
+const getRandomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+const generateRandomLocationsInNola = () => {
+  const latitude = getRandomNumber(
+    nolaBoundaries.minLatitude,
+    nolaBoundaries.maxLatitude
+  );
+  const longitude = getRandomNumber(
+    nolaBoundaries.minLongitude,
+    nolaBoundaries.maxLongitude
+  );
+
+  return {
+    latitude,
+    longitude,
+  };
+};
+
+const createFakeResource = () => {
+  return {
+    resource: resourceArray[Math.floor(Math.random() * resourceArray.length)],
+    location: generateRandomLocationsInNola(),
+    offeredBy: {
+      name: faker.name.findName(),
+      email: faker.internet.email(),
+      phone: faker.phone.phoneNumber(),
+    },
+    availableUntil: faker.date.future(),
+  };
+};
+
+export const firebaseSeeder = async () => {
+  for (let i = 0; i < 100; i++) {
+    fetch(
+      'https://mutual-aid-hackathon-default-rtdb.firebaseio.com/resources.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(createFakeResource()),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+    const fakeResource = createFakeResource();
+    console.log(fakeResource);
+    seedArr.push(fakeResource);
+  }
+};
+
+console.log(seedArr);
--- send_sms.js
+++ send_sms.js
@@ -6,36 +6,88 @@
 //  ngrok http 3000 (to connect to https://6854-174-76-80-43.ngrok.io)
 //  node send_sms.js (to run on 3000)
 //
-require('dotenv').config();
-
-// body parser to parse encoded message data
-const bodyParser = require('body-parser');
 
 // required libraries for node.js
-const express = require('express');
+import express from 'express';
 const app = express();
 const port = 3000;
+
+import dotenv from 'dotenv';
+// require('dotenv').config();
+dotenv.config();
+
+import fetch from 'node-fetch';
+
+import Twilio from 'twilio/lib/rest/Twilio.js';
+import MessagingResponse from 'twilio/lib/twiml/MessagingResponse.js';
+
+// import { firebaseSeeder } from './seed.js';
+// firebaseSeeder();
 
 const accountSid = process.env.TWILIO_SID;
 const authToken = process.env.TWILIO_TOKEN;
 // const twilioNumber= process.env.TWILIO_NUMBER;
 
 // required modules for twilio response
-const client = require('twilio')(accountSid, authToken);
-const MessagingResponse = require('twilio').twiml.MessagingResponse;
+const client = new Twilio(accountSid, authToken);
+// const MessagingResponse = new Twilio.twiml.MessagingResponse();
 
-// Configure body-parser middleware for urlencoded data
-app.use(bodyParser.urlencoded({ extended: false }));
+// test firebase get request for some food resources
+const fetchFoodResources = async () => {
+  // fetch data from firebase
+  const fetchResourceData = async () => {
+    const response = await fetch(
+      'https://mutual-aid-hackathon-default-rtdb.firebaseio.com/resources.json'
+    );
+    if (!response.ok) {
+      throw new Error('Could not fetch cart data');
+    }
+    const data = await response.json();
+    return data;
+  };
+
+  try {
+    let foodResourceData = [];
+    const resourceData = await fetchResourceData();
+
+    // filter for food resources
+    for (let key in resourceData) {
+      if (resourceData[key].resource === 'Food') {
+        foodResourceData.push(resourceData[key]);
+      }
+    }
+
+    // grab first 4 food resources
+    foodResourceData = foodResourceData.slice(0, 4);
+
+    // format text response
+    let returnedFoodReesources = foodResourceData.map((resource) => {
+      return `Location: ${resource.location.latitude}, ${resource.location.longitude} \nOffered By: ${resource.offeredBy.name} \nContact: ${resource.offeredBy.phone} \nAvailable Until: ${resource.availableUntil} \n\n`;
+    });
+
+    let returnText = `There are ${foodResourceData.length} food resources available in New Orleans near you. Here they are from closest to farthest: \n\n ${returnedFoodReesources} \n\n If you would like to see more resources, please text 'more'}`;
+
+    // log text response (still need to send to user)
+    // console.log('returnText \n\n', returnText);
+    return returnText;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const foodTextResponse = await fetchFoodResources();
+console.log('foodTextResponse', foodTextResponse);
 
 // webhook to handle response
 app.post('/message-rene', async (req, res) => {
-  console.log(`req`, req.body.Body);
-
+  console.log(`req`, req.body);
   let textResponse;
-  if (req.body.Body) {
-    twiml.message('this is a message from us made in node');
+  if (req.body.Body === 'food' || req.body.Body === 'Food') {
+    twiml.message(
+      foodTextResponse ||
+        'There are no food resources available in New Orleans near you. Please try again later.'
+    );
   }
-
   const twiml = new MessagingResponse();
   res.writeHead(200, { 'Content-Type': 'text/xml' });
   res.end(twiml.toString());
@@ -44,11 +96,3 @@ app.post('/message-rene', async (req, res) => {
 app.listen(3000, () => {
   console.log('Express server listening on port 3000');
 });
-
-// client.messages
-//   .create({
-//      body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-//      from: '+18772140953',
-//      to: '+15048587964'
-//    })
-//   .then(message => console.log(message.sid));
